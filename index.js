29f095d53e721f5665e148e28dd5e48c02c60cc0
KingSaad <110625272+aseedasaad@users.noreply.github.com>
2023-12-03 12:07:35 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,21 @@
+const statusText = document.querySelector("statusText");
+
+function initializeGame(){
+    statusText.textContent = `${playerOne}'s turn`;
+}
+initializeGame();
+
+let playerOne = "X";
+
+let playerTwo = "O";
+
+const winConditions = [  
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],   
+]
