e5abffe8fc9cce063bf8644026ec1ca13c9f5772
KingSaad <110625272+aseedasaad@users.noreply.github.com>
2023-12-05 13:32:16 -0500

    Improvements

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-const statusText = document.querySelector("statusText");
+const playerOne = "X";
+const playerTwo = "O";
+const statusText = document.querySelector("#statusText");
+const startBtn = document.querySelector("#startBtn");
+const restartBtn = document.querySelector("#restartBtn");
 
-function initializeGame(){
-    button = document.querySelector("startBtn");
-    statusText.textContent = `${playerOne}'s turn`;
-    startBtn.addEventListener("click", initializeGame);
+let currentPlayer = playerOne;
+
+let boardState = new Array(3).fill(new Array(3).fill(null));
+
+function initializeGame() {
+  statusText.textContent = `${currentPlayer}'s turn`;
 }
-initializeGame();
 
-let playerOne = "X";
-
-let playerTwo = "O";
-
-const winConditions = [  
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],   
-]
-
-function winner(){
-    if (playerOne === winConditions){
-        return (`${playerOne} has won the game!`);
-    } else if (playerTwo === winConditions){
-        return (`${playerTwo} has won the game!`);
-    } 
+function playerTurn() {
+  if (currentPlayer === playerOne) {
+    currentPlayer = playerTwo;
+  } else {
+    currentPlayer = playerOne;
+  }
 }
-winner();
 
-function endGame(){
-    button1 = document.querySelector("restartBtn");
-    restartBtn.addEventListener("click", endGame);
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function winner() {
+  if (playerOne === winConditions) {
+    return `${playerOne} has won the game!`;
+  } else if (playerTwo === winConditions) {
+    return `${playerTwo} has won the game!`;
+  }
 }
-endGame();
+
+function endGame() {
+    statusText.textContent = "Game Ended";
+}
+startBtn.addEventListener("click", initializeGame);
+restartBtn.addEventListener("click", endGame);
